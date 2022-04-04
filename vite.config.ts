9222267113a45ad3239a9d35e8b5711d02098b5c
Sangg <ngocsangyem@gmail.com>
2022-04-04 16:43:56 +0700

    :wrench:  config alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@/': resolve(__dirname, './src'),
+			'@/components': resolve(__dirname, './src/components'),
+			'@/@types': resolve(__dirname, './src/@types'),
+			'@/views': resolve(__dirname, './src/views'),
+			'@/assets': resolve(__dirname, './src/assets'),
+		},
+	},
+});
