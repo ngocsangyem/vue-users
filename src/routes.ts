115ec415f6413c3a09d03fbc668feb0326eea97d
Sangg <ngocsangyem@gmail.com>
2022-04-04 16:34:19 +0700

    :ambulance: fix routes path

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
-import Home from './views/Home.vue';
+import Home from './views/Home/Home.vue';
 
 export const routes: RouteRecordRaw[] = [
 	{
